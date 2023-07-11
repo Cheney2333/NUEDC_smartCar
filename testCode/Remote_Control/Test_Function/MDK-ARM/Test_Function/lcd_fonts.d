f251756d5517a8537f468c44341001892552454b
Cheney2333 <space0424@foxmail.com>
2023-07-11 21:24:49 +0800

    ADC功能测试，使用了F407

---
+++ testCode/Remote_Control/Test_Function/MDK-ARM/Test_Function/lcd_fonts.d
@@ -0,0 +1,3 @@
+test_function/lcd_fonts.o: ..\HARDWARE\LCD_SPI_169\lcd_fonts.c \
+  ..\HARDWARE\LCD_SPI_169\lcd_fonts.h \
+  D:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
---
+++ testCode/Remote_Control/Test_Function/MDK-ARM/Test_Function/lcd_image.d
@@ -0,0 +1,3 @@
+test_function/lcd_image.o: ..\HARDWARE\LCD_SPI_169\lcd_image.c \
+  ..\HARDWARE\LCD_SPI_169\lcd_image.h \
+  D:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
